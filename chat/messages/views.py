0be99f3140c2e741a0963f49b27c0c057c673cf4
Taylor Burgess <tayloraburgess@gmail.com>
2016-10-18 01:33:45 -0400

    Add POST to api/v1/messages
    Also add various status code responses for invalid requests. Temporarily
    make route csrf_exempt so RESTful POST requests ill work, but find a
    better way to do this with proper authentication in a later commit.

--- chat/messages/views.py
+++ chat/messages/views.py
@@ -1,15 +1,38 @@
+import json
+
 from django.core import serializers
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt 
 
 from .models import Message
 from django.contrib.auth.models import User
 
+@csrf_exempt
 def all(request):
-    data = serializers.serialize('json', Message.objects.all())
-    return HttpResponse(data)
+    if request.method == 'GET':
+        data = serializers.serialize('json', Message.objects.all())
+        return HttpResponse(data, content_type='application/json')
+    elif request.method == 'POST':
+        if request.content_type == 'application/json':
+            body = json.loads(request.body.decode('utf-8'))
+            new_message = Message.objects.create(
+                text=body['text'],
+                date_sent=int(body['date_sent']),
+                author=User.objects.get(pk=body['author']),
+                recipient=User.objects.get(pk=body['recipient'])
+            )
+            new_message.save()
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=415)
+    else:
+       return HttpResponse(status=405)
 
 def detail(request, message_id):
-    message = get_object_or_404(Message, pk=message_id)
-    data = serializers.serialize("json", [message])
-    return HttpResponse(data)
+    if request.method == 'GET':
+        message = get_object_or_404(Message, pk=message_id)
+        data = serializers.serialize("json", [message])
+        return HttpResponse(data)
+    else:
+        return HttpResponse(status=405)
