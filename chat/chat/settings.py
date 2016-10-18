ef0093cdc1df94e9bcf30c7f2fad997e927692f8
Taylor Burgess <tayloraburgess@gmail.com>
2016-10-17 22:58:00 -0400

    Modify settings.py for environment
    Change app to look for a sqlite filename in the
    environment, rather than using the hardcoded
    'db.sqlite3' value.

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'chat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, os.environ['DATABASE_FILE']),
     }
 }
 
