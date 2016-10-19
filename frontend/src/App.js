a9d22407ebbacc5d79bd9517dac8485175abb7f7
Taylor Burgess <tayloraburgess@gmail.com>
2016-10-18 23:10:42 -0400

    Add callback to switch streams
    Allow user to click on other users to switch the view to their
    messages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,34 +33,59 @@ const SPA = React.createClass({
     getInitialState: function() {
         return {
             userPk: 3,
-            currentStream: 2,
+            currentStream: 0,
             friendList: [],
-            messageList: []
+            streamsDict: {}
         };
     },
 
     componentDidMount: function() {
-        const friendsURL = `/api/v1/users/${this.state.userPk}/friends`
-        const streamURL = `/api/v1/users/${this.state.userPk}/stream/${this.state.currentStream}`
-        this.serverRequest = $.get(streamURL, (res) => {
-            const messages = getMessageList(res);
-            this.setState({
-                messageList: messages
-            });
-        });
-        this.serverRequest =  $.get(friendsURL, (res) => {
-           const users = getUserList(res);
-           this.setState({
-               friendList: users  
-           }); 
+        const friendsURL = `/api/v1/users/${this.state.userPk}/friends`;
+        $.ajax({
+            type: 'GET',
+            url: friendsURL,
+            success: (res1) => {
+                const users = getUserList(res1);
+                this.setState({
+                    friendList: users
+                });
+               this.state.friendList.forEach((user) => {
+                   const streamURL = `/api/v1/users/${this.state.userPk}/stream/${user.pk}`;
+                   $.ajax({
+                       type: 'GET',
+                       url: streamURL,
+                       success: (res) => {
+                           const newState = Object.assign({}, this.state.streamsDict);
+                           newState[user.pk] = getMessageList(res);
+                           this.setState({
+                               streamsDict: newState, 
+                               currentStream: user.pk
+                           });
+                        }
+                   });
+                });  
+            } 
         });
     },
 
+    changeStream: function(userPk) {
+        this.setState({
+            currentStream: userPk 
+       });
+    },
+
+
     render: function() {
+        let messageList;
+        if (Object.keys(this.state.streamsDict).length > 0) {
+            messageList = this.state.streamsDict[this.state.currentStream];
+        } else {
+            messageList = [];
+        }
         return (
             <div>
-                <Friends friendList={this.state.friendList} />
-                <Messages messageList={this.state.messageList} />
+                <Friends friendList={this.state.friendList} changeStream={this.changeStream}/>
+                <Messages messageList={messageList} />
             </div>
         );
     } 
@@ -83,20 +108,33 @@ const Messages = React.createClass({
 
 const Friends = React.createClass({
     render: function() {
-        const names = this.props.friendList.map((friend) => {
-            return (
-               <li>{ friend.username }</li> 
-            );
-        });
- 
-        return(
+        return (
             <div>
                 <ul>
-                    {names}
+                    {
+                       this.props.friendList.map((friend) => {
+                           return (
+                               <Friend data={friend} changeStream={this.props.changeStream}/> 
+                            );
+                        })
+                    }
                 </ul>
             </div>
         );
     }
 });
+
+const Friend = React.createClass({
+    render: function() {
+        const changeStream = () =>  {
+            this.props.changeStream(this.props.data.pk)
+        };
+        return (
+            <li> <a href="#" onClick={changeStream}>{ this.props.data.username }</a></li>
+        )
+    }
+})
+
+
 
 export default App;
